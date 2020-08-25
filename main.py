0c34c5237335f4ef7b5a3ff028d92f95e4d6ade1
fralychen <fralychen@gmail.com>
2020-08-25 10:23:18 +0800

    add keepalive

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 '''
 bot = Bot(console_qr=True)
 kele = bot.friends().search('可乐')[0]
+#assistant = bot.friends().search('文件传输助手')[0]
+
 sched = BlockingScheduler()
 
 
@@ -30,14 +32,19 @@ def send_message():
     message = "{}\n{}\n{}\n".format(title,author,origin)
     kele.send(message)   #发送消息
 
+def keep_alive():
+    single = ""
+    kele.send(single)
 
-def cron(event):
+def cron(event,keep_alive):
     '''
     周一至周五早上8：20执行任务
     '''
     sched = BlockingScheduler()
-    sched.add_job(event, 'cron',  day_of_week='1-5', hour=8, minute=20)
+    sched.add_job(event, 'cron',  day_of_week='1-5', hour=16, minute=23)
+    sched.add_job(keep_alive, 'interval', hours=1)
     sched.start()
     
 if __name__ == "__main__":
-    cron(send_message)
+    #send_message()
+    cron(send_message,keep_alive)
